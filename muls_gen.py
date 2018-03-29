8eb4857c9133de48b0148583bc5d405bf0183c93
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2018-03-29 23:49:02 +0200

    Add BBP15 in muls_gen.py

--- muls_gen.py
+++ muls_gen.py
@@ -55,7 +55,58 @@ def isw(d):
 
     return c
 
-muls = {'isw': isw}
+def BBP15(d):
+    d2 = d-1
+    c = circuit_model.Circuit()
+    _, _, _, x, y, z = mul_preamble(d, c)
+
+    # product matrix
+    p = [[c.var(f'p_{i}_{j}') for j in range(d)] for i in range(d)]
+    for i, j in it.product(range(d), range(d)):
+        c.l_prod(p[i][j], (x[i], y[j]))
+
+    # randoms & temps in matrix & compression
+    r = [
+            {d2-j: c.var(f'r_{i}_{j}', kind='random') for j in range(d2-i)}
+            for i in range(d)
+            ]
+    r2 = {j: c.var(f'r2_{j}', kind='random') for j in range(d2-1, 0, -2)}
+    t = [
+            {j: [c.var(f't_{i}_{j}_{k}') for k in range(5)]
+                    for j in range(d2, i+1, -2)}
+            for i in range(d)
+            ]
+    c_var = [{j: c.var(f'c_{i}_{j}') for j in range(d2+2, i+1, -2)}
+            for i in range(d)
+            ]
+    for i in range(d):
+        c.p_sum(c_var[i][d2+2], (p[i][i],))
+        for j in range(d2, i+1, -2):
+            c.l_sum(t[i][j][0], (r[i][j], p[i][j]))
+            c.l_sum(t[i][j][1], (t[i][j][0], p[j][i]))
+            c.l_sum(t[i][j][2], (t[i][j][1], r2[j-1]))
+            c.l_sum(t[i][j][3], (t[i][j][2], p[i][j-1]))
+            c.l_sum(t[i][j][4], (t[i][j][3], p[j-1][i]))
+            c.l_sum(c_var[i][j], (c_var[i][j+2], t[i][j][4]))
+        if (i-d2) % 2 != 0:
+            t[i][i+1] = [c.var(f't_{i}_{i+1}_0'), c.var(f't_{i}_{i+1}_1')]
+            c_var[i][i+1] = c.var(f'c_{i}_{i+1}')
+            c.l_sum(t[i][i+1][0], (r[i][i+1], p[i][i+1]))
+            c.l_sum(t[i][i+1][1], (t[i][i+1][0], p[i+1][i]))
+            c.l_sum(c_var[i][i+1], (c_var[i][i+3], t[i][i+1][1]))
+            if i % 2 == 1:
+                c.l_sum(z[i], (c_var[i][i+1], r2[i]))
+            else:
+                c.p_sum(z[i], (c_var[i][i+1],))
+        else:
+            for j in range(i-1, -1, -1):
+                c_var[i][j+2] = c.var(f'c_{i}_{j+2}')
+                c.l_sum(c_var[i][j+2], (c_var[i][j+3], r[j][i]))
+            c.p_sum(z[i], (c_var[i][2],))
+
+    return c
+
+muls = {'isw': isw, 'BBP15': BBP15}
 
 import sys
 
@@ -86,10 +137,12 @@ if __name__ == '__main__':
     except IndexError:
         d = 3
     #gen_all(d)
-    print(f'---- ISW, d={d} ----')
-    print(isw(d))
-    for _ in range(100):
-        test_mul(d, isw)
+    #for mul_name, mul_f in muls.items():
+    for mul_name, mul_f in [('BBP15', BBP15)]:
+        print(f'---- {mul_name}, d={d} ----')
+        print(mul_f(d))
+        for _ in range(100):
+            test_mul(d, mul_f)
 
     #print(f'---- BBP15, d={d} ----')
     #print(BBP15(d))
