5af74ca866f659496010500f51670e6b6f4ee78d
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2018-06-03 22:55:47 +0200

    Cleanup obs_mi

--- obs_mi.py
+++ obs_mi.py
@@ -10,23 +10,30 @@ mem = joblib.Memory(cachedir='cache',
         )
 
 @mem.cache
-def compute_target_mis(obs_mis, c_name,  d, tol=1e-5, var_name='x'):
+def gen_lkm(c_name, d, var_name):
     c = muls_gen.muls[c_name](d)
     abs_c_idx, var_map = c.to_lkm(rand_var_leak=1, only_inputs_leak=False)
     v = c.fmt_var([var for var in c.vars if var.name == var_name][0])
     x_idx = var_map[v]
+    return abs_c_idx, x_idx
+
+@mem.cache
+def compute_target_mis(obs_mis, c_name,  d, tol=1e-5, var_name='x'):
+    abs_c_idx, x_idx = gen_lkm(c_name, d, var_name)
     pfg = librfactor_python.PyFactorGraph(*abs_c_idx)
     return [pfg.bp_mi(obs_mi, 1, tol, 1.0, 1.0, 10000, False)[0][x_idx]
             for obs_mi in obs_mis]
 
-def compute_target_mis2(obs_mis, circuit, tol=1e-5, var_name='x'):
-    c = circuit
-    abs_c_idx, var_map = c.to_lkm(rand_var_leak=1, only_inputs_leak=False)
-    v = c.fmt_var([var for var in c.vars if var.name == var_name][0])
-    x_idx = var_map[v]
-    pfg = librfactor_python.PyFactorGraph(*abs_c_idx)
-    return [pfg.bp_mi(obs_mi, 1, tol, 1.0, 1.0, 10000, False)[0][x_idx]
-            for obs_mi in obs_mis]
+@mem.cache
+def compute_target_mis_orders(obs_mi, c_name,  ds, tol=1e-5, var_name='x'):
+    res = []
+    for d in ds:
+        abs_c_idx, x_idx = gen_lkm(c_name, d, var_name)
+        pfg = librfactor_python.PyFactorGraph(*abs_c_idx)
+        res.append(
+                pfg.bp_mi(obs_mi, 1, tol, 1.0, 1.0, 10000, False)[0][x_idx]
+                )
+    return res
 
 @mem.cache
 def ub_mi(obs_mis, c_name,  d, tol=1e-3, var_name='x'):
